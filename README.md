a0ce029575623a67970eda9a7adf8da6ad53b523
kearmododragon <ckearney1992@gmail.com>
2023-03-01 01:28:27 +0100

    almost complete TTT, need to make pretty

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Tic-Tac-Toe
 Tic Tac Toe made by Ciaran
+
+
+make a division for each winning selection (have one for rows already)
+
--- main.js
+++ main.js
@@ -1,86 +1,21 @@
-// NOTES ON WHAT THE PLAN IS // 
-// - Display an empty tic-tac-toe board when the page is initially displayed.
-//- A player can click on the nine cells to make a move.
-// Every click will alternate between marking an `X` and `O`.
-//- Once occupied with an `X` or `O`, the cell cannot be played again. 
-//{REMOVE EVENT LISTENER }
-//- Provide a `Reset Game` button that will clear the contents of the board.
-
-
-
-// PSEUDOCODE start //
-/*
-Array solution could be best option
-
-4) Upon loading the app should:
-  4.1) Initialize the state variables:
-    4.1.1) Initialize the board array to 9 nulls to represent empty squares. The 9 elements will "map" to each square, where index 0 maps to the top-left 
-    square and index 8 maps to the bottom-right square.
-    4.1.2) Initialize whose turn it is to 1 (player 'X'). Player 'O' will be represented by -1.
-    4.1.3) Initialize winner to null to represent that there is no winner or tie yet. Winner will hold the player value (1 or -1) if there's a winner. Winner will hold a 'T' if there's a tie.
-  4.2) Render those state variables to the page:
-    4.2.1) Render the board:
-      4.2.1.1) Loop over each of the 9 elements that represent the squares on the page, and for each iteration:
-        4.2.1.1.2) Use the index of the iteration to access the mapped value from the board array.
-        4.3.1.1.3) Set the background color of the current element by using the value as a key on the colors lookup object (constant).
-    4.2.2) Render a message:
-      4.2.2.1) If winner has a value other than null (game still in progress), render whose turn it is - use the color name for the player, converting it to upper case.
-      4.2.2.2) If winner is equal to 'T' (tie), render a tie message.
-      4.2.2.3) Otherwise, render a congratulatory message to which player has won - use the color name for the player, converting it to uppercase.
-  4.3) Wait for the user to click a square
-
-
-
-5) Handle a player clicking a square:
-  5.1) Obtain the index of the square that was clicked by either:
-    5.1.1) "Extracting" the index from an id assigned to the element in the HTML, or
-    5.1.2) Looping through the cached square elements using a for loop and breaking out when the current square element equals the event object's target.
-  5.2) If the board has a value at the index, immediately return because that square is already taken.
-  5.3) If winner is not null, immediately return because the game is over.
-  5.4) Update the board array at the index with the value of turn.
-  5.5) Flip turns by multiplying turn by -1 (flips a 1 to -1, and vice-versa).
-  5.6) Set the winner variable if there's a winner:
-    5.6.1) Loop through the each of the winning combination arrays defined.
-    5.6.2) Total up the three board positions using the three indexes in the current combo.
-    5.6.3) Convert the total to an absolute value (convert any negative total to positive).
-    5.6.4) If the total equals 3, we have a winner! Set winner to the board's value at the index specified by the first index in the combo array. Exit the loop.
-  5.7) If there's no winner, check if there's a tie:
-    5.7.1) Set winner to 'T' if there are no more nulls in the board array.
-  5.8) All state has been updated, so render the state to the page (step 4.2).
-
-
-
-
-
-6) Handle a player clicking the replay button:
-  6.1) Do steps 4.1 (initialize the state variables) and 4.2 (render).
-*/
-
-
-
 /* --- CONSTANTS --- */
 const colors ={
-    null: "null",
-    player1: "1",
-    player2: "-1",
+    empty: "#ffffff",
+    playerX: "#ff0000",
+    playerO: "#00ff00",
 }
-
 const win = [
-    [1, 2, 3],
-    [4, 5, 6],
-    [7, 8, 9],
+    [0, 1, 2,],
+    [3, 4, 5,],
+    [6, 7, 8,],
+    [0, 3, 6,],
     [1, 4, 7],
     [2, 5, 8],
-    [3, 5, 7],
-    [4, 5, 6],
-    [1, 5, 9],
+    [0, 4, 8,],
+    [2, 4, 6,],
 ]
 
-const board = [
-        [1, 2, 3],
-        [4, 5, 6],
-        [7, 8, 9],
-]
+const board = [0, 0, 0, 0, 0, 0, 0, 0, 0]
 
 // have board locations and winning formula, will give values by using indexes //
 // check box hasn't been played yet //
@@ -95,9 +30,10 @@ const squares = document.querySelectorAll("#squares")
 
 /* --- STATE VARIABLES ---*/
 squareStatus = 0
-let turn
+let turn = 0
+let currentPlayer = "X"
 let boardy
-let winner
+let winner = "none"
 
 /* --- CACHED ELEMENTS---*/
 
@@ -105,29 +41,76 @@ const boardEl = document.querySelector("#board")
 const squareEls = document.querySelectorAll("div.square")
 
 /* --- FUNCTIONS---*/
+
 function render(){
-    console.log("rendering game")
+  for (var i=0; i<board.length; i++) {
+    if (board[i] === "X"){
+      document.getElementById(i).style.backgroundColor = colors.playerX
+      document.getElementById(i).innerHTML = "X"
+    } else if (board[i] === "O"){
+      document.getElementById(i).style.backgroundColor = colors.playerO
+      document.getElementById(i).innerHTML = "O"
+    } else {
+      document.getElementById(i).style.backgroundColor = colors.empty
+      document.getElementById(i).innerHTML = ""
+    }
+  }
 }
 
-function init(){
-console.log("starting game")
-for (square of squareEls){
-    square.classList.contains("active")
-    square.classlist.add("inactive")
-}
-render()
+
+
+function isSquareEmpty(boxIndex){
+  return board[boxIndex] === 0
 }
 
-
-function setSquare(evt) {
-//squareStatus
-
+function setSquare(box){
+  board[box.id]=currentPlayer
+  render()
 }
 
-/* --- EVENT LISTENERS ---*/
+function isWinner(){
+  for (var i=0; i<win.length; i++){
+    if (
+          board[win[i][0]] === currentPlayer && 
+          board[win[i][1]] === currentPlayer && 
+          board[win[i][2]] === currentPlayer 
+        ) { 
+            return true
+          }
+  } return false
+}
+function resetBoard(){
+  for (var i=0; i<board.length; i++){
+    board[i] = 0
+  } 
+  winner = "none"
+  document.getElementById("header").innerHTML = "Tic Tac Toe"
+  render()
+}
 
-//boardEl.addEventListener("click", setSquare)
-//btn.removeEventListener("click", handleClick);
+function tie() {
+  document.getElementById("header").innerHTML = "It's a TIE"
+}
 
+function squareClicked(box) {
+if (isSquareEmpty(box.id) && winner === "none"){
+    setSquare(box)
+    if (isWinner()){
+      winner = currentPlayer
+      console.log("game won: " + currentPlayer);
+      document.getElementById("header").innerHTML = "Winner is " + winner
+      //this runs when there's a winner
+      // advise players who wins
+      // if tie advise tie
+      // add winner is box?
+      }
+    currentPlayer = currentPlayer === "X" ? "O" : "X"
+    turn++
+    if (turn ===  9){
+      tie()
+    }
 
-// 
+} else {
+    console.log ("square's full")
+}
+}
